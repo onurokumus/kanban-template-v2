5835e60430e09d624c4743649892b59d27eb333e
unknown <onur.okumus@hotmail.com>
2026-03-18 04:57:51 -0400

    Add allowedHosts to vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/k11c0_kezban/',
+  server: {
+    allowedHosts: ['heliweb1', 'heliweb1.dmntai.intra'],
+  },
 })
